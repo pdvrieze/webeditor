16975d1735355ebf6839697362cb9db6e2032cbe
sQu1rr <sQu1rr1c@gmail.com>
2016-04-29 19:40:14 +0100

    Fixed jQuery path
    Dialogue cannot be dismissed with backdrop any more

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ define(function () {
     require.config({
         baseUrl: 'js/',
         paths: {
-            jquery: '../bower_components/jquery/dist/jquery',
+            jquery: '../bower_components/jquery/jquery',
             bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
             joint: '../bower_components/jointjs/dist/joint',
             lodash: '../bower_components/lodash/lodash',
